d6130a059505a72500fe6adbc388394da4aefa46
= <amberoortiz@gmail.com>
2024-09-18 11:51:38 -0700

    20% complted

--- Mini Project/src/app/page.tsx
+++ Mini Project/src/app/page.tsx
@@ -1,7 +1,10 @@
+import Background from "@/components/bg";
+
 export default function App () {
   return (
-      <div className="w-full h-screen bg-black text-white flex items-center justify-center ">
-        <h1 className="font-bold text-5xl text-center underline">Folders Check In Multiples Projects' <br /> Enjoy Your Self </h1>
-      </div>
+    <div className="relative w-full h-screen bg-zinc-800">
+      <Background />
+      <div className="w-full h-full bg-red-400"></div>
+    </div>
   )
 }
