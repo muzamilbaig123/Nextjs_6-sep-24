be1d70a8f7584abe03e575c54c8840444e847d68
= <amberoortiz@gmail.com>
2024-09-18 10:37:34 -0700

    mini project folder added

---
+++ Mini Project/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function App () {
+  return (
+      <div className="w-full h-screen bg-black text-white flex items-center justify-center ">
+        <h1 className="font-bold text-5xl text-center underline">Folders Check In Multiples Projects' <br /> Enjoy Your Self </h1>
+      </div>
+  )
+}
