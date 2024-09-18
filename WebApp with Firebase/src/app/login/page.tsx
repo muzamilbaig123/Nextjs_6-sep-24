74471f370375e0dff8f19be9dbf74b1693b16394
= <amberoortiz@gmail.com>
2024-09-18 04:39:44 -0700

    firebase activated

---
+++ WebApp with Firebase/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LogIn () {
+    return (
+        <>
+            <h1>LogIn</h1>            
+        </>
+    )
+}
---
+++ WebApp with Firebase/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home () {
+  return (
+    <>
+      <h1 className="font-bold text-4xl text-center">Home Page</h1>
+    </>
+  )
+}
