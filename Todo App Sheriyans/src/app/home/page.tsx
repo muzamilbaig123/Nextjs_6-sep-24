40b90af606cd56a2e77fa2f6ff34c41a45242aef
= <amberoortiz@gmail.com>
2024-11-07 11:30:28 -0800

    just old authentication

--- Todo App Sheriyans/src/app/home/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-export default function Home () {
-  return (
-    <>
-    <h1 className="font-bold text-center underline text-4xl">Student Management Portal</h1>
-    </>
-  )
-}
--- TodoApp/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <>
-            <div className="logo bg-blue-800 text-center">
-                <h2 className="text-white text-2xl p-4 font-normal cursor-pointer">Copyright © 2024 llc All rights reserved</h2>
-            </div>
-        </>
-    )
-}
